f2b723051a58d95f5db8e676859b6864bdeee039
Joseph <ayanfe.dev@gmail.com>
2026-06-25 21:11:55 +0100

    feat: user entity

---
+++ Entities/User.cs
@@ -0,0 +1,11 @@
+namespace LifeTimelineApi.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = "";
+
+    public string PasswordHash { get; set; } = "";
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
