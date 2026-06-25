e3348646b9650f61b3a31530e77d70788ceac635
Joseph <ayanfe.dev@gmail.com>
2026-06-26 00:31:02 +0100

    feat: create milestone request dto

---
+++ Dtos/CreateMilestoneRequest.cs
@@ -0,0 +1,12 @@
+public class CreateMilestoneRequest
+{
+    public string Title { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public string Emoji { get; set; } = "";
+
+    public string Mood { get; set; } = "";
+
+    public DateTime Date { get; set; }
+}
