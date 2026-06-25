b2ea97c010af9d20f1f308498a0f5c53f574133d
Joseph <ayanfe.dev@gmail.com>
2026-06-26 00:59:49 +0100

    fix: auth routes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/auth")]
 public class AuthController : ControllerBase
 {
     private readonly AppDbContext _dbContext;
