01be97ce71c6a9c816e387234c87ccde3656fe41
Joseph <ayanfe.dev@gmail.com>
2026-06-27 23:13:43 +0100

    feat: created milestone stats dto

---
+++ Application/Dtos/Milestone/MilestoneStatsDto.cs
@@ -0,0 +1,6 @@
+public class MilestoneStatsDto
+{
+    public int TotalMilestones { get; set; }
+    public int TotalMilestonesThisMonth { get; set; }
+    public int TotalMilestonesThisYear { get; set; }
+}
