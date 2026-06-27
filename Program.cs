2c7d4e610214ba3a3caa54ea7b7f99ba9cb61017
Joseph <ayanfe.dev@gmail.com>
2026-06-27 22:37:04 +0100

    feat: register milestone service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using LifeTimelineApi.Data;
+using LifeTimelineApi.Application.Services.Milestones;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IMilestoneService, MilestoneService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
