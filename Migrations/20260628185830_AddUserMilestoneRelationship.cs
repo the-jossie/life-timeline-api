79aa81d690a1247776fff10d405cab8a61909f1f
Joseph <ayanfe.dev@gmail.com>
2026-06-28 20:07:19 +0100

    chore: update migrations

---
+++ Migrations/20260628185830_AddUserMilestoneRelationship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace life_timeline_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserMilestoneRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
